4949e4225f051bf6a5a4a8777d8b8b4eea2f4f7d
AidanCuff <66979339+AidanCuff@users.noreply.github.com>
2023-03-12 21:52:59 -0400

    Update run_mininet.py

--- run_mininet.py
+++ run_mininet.py
@@ -6,103 +6,102 @@ from mininet.node import CPULimitedHost
 from mininet.link import TCLink
 import sys
 
-with open('output.txt', 'w') as sys.stdout:
-    class SingleSwitchTopo(Topo):
-        def __init__(self,n):
-            Topo.__init__(self,n)
-            switch = self.addSwitch('s1')
+class SingleSwitchTopo(Topo):
+    def __init__(self,n):
+        Topo.__init__(self,n)
+        switch = self.addSwitch('s1')
 
-            hosts = [self.addHost(f'h{i+1}',cpu=.5/n) for i in range(n)]
-            for j in range(n):
-                self.addLink(hosts[j], switch, bw=10, delay='5ms', loss=10, max_queue_size=1000)
+        hosts = [self.addHost(f'h{i+1}',cpu=.5/n) for i in range(n)]
+        for j in range(n):
+            self.addLink(hosts[j], switch, bw=10, delay='5ms', loss=10, max_queue_size=1000)
 
-    class LinearTopo(Topo):
-        def __init__(self,n):
-            Topo.__init__(self)
+class LinearTopo(Topo):
+    def __init__(self,n):
+        Topo.__init__(self)
 
-            # Add switches to the topology
-            switches = [self.addSwitch(f's{i+1}') for i in range(n)]
+        # Add switches to the topology
+        switches = [self.addSwitch(f's{i+1}') for i in range(n)]
 
-            # Add hosts to the topology
-            hosts = [self.addHost(f'h{j+1}', cpu=.5/n) for j in range(n)]
+        # Add hosts to the topology
+        hosts = [self.addHost(f'h{j+1}', cpu=.5/n) for j in range(n)]
 
-            # Add links between the switches and hosts
-            for i in range(n):
-                self.addLink(hosts[i], switches[i], bw=10, delay='5ms', loss=10, max_queue_size=1000)
-                if i < n-1:
-                    self.addLink(switches[i], switches[i+1], bw=10, delay='5ms', loss=10, max_queue_size=1000)
-
-
-    class TreeTopo(Topo):
-        def __init__(self, depth):
-            Topo.__init__(self,depth)
-
-            #Assign variables, add root switch
-            a, b, s_count, h_count, snh = 1, 2, 2, 1, [self.addSwitch('s1')]
-
-            #Construct tree and add links
-            for i in range((2**(depth+1)-1)//2):
-
-                if i >= ((2**depth)//2)-1:
-                    snh.append(self.addHost(f'h{h_count}', cpu=.5/depth))
-                    snh.append(self.addHost(f'h{h_count+1}', cpu=.5/depth))
-                    h_count += 2
-
-                else:
-                    snh.append(self.addSwitch(f's{s_count}'))
-                    snh.append(self.addSwitch(f's{s_count+1}'))
-                    s_count += 2
-
-                self.addLink(snh[i],snh[a+i], bw=10, delay='5ms', loss=10, max_queue_size=1000)
-                self.addLink(snh[i],snh[b+i], bw=10, delay='5ms', loss=10, max_queue_size=1000)
-                a, b = b, b+1
+        # Add links between the switches and hosts
+        for i in range(n):
+            self.addLink(hosts[i], switches[i], bw=10, delay='5ms', loss=10, max_queue_size=1000)
+            if i < n-1:
+                self.addLink(switches[i], switches[i+1], bw=10, delay='5ms', loss=10, max_queue_size=1000)
 
 
+class TreeTopo(Topo):
+    def __init__(self, depth):
+        Topo.__init__(self,depth)
 
-    class MeshTopo(Topo):
-        def __init__(self,n):
-            Topo.__init__(self,n)
+        #Assign variables, add root switch
+        a, b, s_count, h_count, snh = 1, 2, 2, 1, [self.addSwitch('s1')]
 
-            # Add switches to the topology
-            switches = [self.addSwitch(f's{i+1}') for i in range(n)]       
-            # Add hosts to the topology
-            hosts = [self.addHost(f'h{j+1}', cpu=.5/n) for j in range(n)]
+        #Construct tree and add links
+        for i in range((2**(depth+1)-1)//2):
 
-            # Add links between switches
-            for i in range(n):
-                for j in range(i+1, n):
-                    self.addLink(switches[i], switches[j], bw=10, delay='5ms', loss=10, max_queue_size=1000)
+            if i >= ((2**depth)//2)-1:
+                snh.append(self.addHost(f'h{h_count}', cpu=.5/depth))
+                snh.append(self.addHost(f'h{h_count+1}', cpu=.5/depth))
+                h_count += 2
 
-            # Add links between hosts and switches
-            for i in range(n):
-                self.addLink(hosts[i], switches[i], bw=10, delay='5ms', loss=10, max_queue_size=1000)
+            else:
+                snh.append(self.addSwitch(f's{s_count}'))
+                snh.append(self.addSwitch(f's{s_count+1}'))
+                s_count += 2
+
+            self.addLink(snh[i],snh[a+i], bw=10, delay='5ms', loss=10, max_queue_size=1000)
+            self.addLink(snh[i],snh[b+i], bw=10, delay='5ms', loss=10, max_queue_size=1000)
+            a, b = b, b+1
 
 
-    def perfTest():
-        if sys.argv[1] == "single":
-            topo = SingleSwitchTopo(int(sys.argv[2]))
-        elif sys.argv[1] =="linear":
-                topo = LinearTopo(int(sys.argv[2]))
-        elif sys.argv[1] =="tree":
-                topo = TreeTopo(int(sys.argv[2]))
-        elif sys.argv[1] =="mesh":
-                topo = MeshTopo(int(sys.argv[2]))
-        else:
-                print("unknown topology")
 
-        net = Mininet(topo, host = CPULimitedHost, link = TCLink)
-        net.start()
-        print("\n dumping host connections: \n")
-        dumpNodeConnections(net.hosts)
-        print("\n Testing network connectivity: \n")
-        net.pingAll();
-        print("\n Testing all pairwise bandwiths between hosts: \n")
-        for i in range(len(net.hosts)):
-                for j in range(i+1, len(net.hosts)):
-                    net.iperf((net.get(f'h{i+1}'),net.get(f'h{j+1}')))
-        print("\n Stopping Network \n")
-        net.stop()
+class MeshTopo(Topo):
+    def __init__(self,n):
+        Topo.__init__(self,n)
 
-    if __name__=='__main__':
-        setLogLevel('info')
-        perfTest()
+        # Add switches to the topology
+        switches = [self.addSwitch(f's{i+1}') for i in range(n)]       
+        # Add hosts to the topology
+        hosts = [self.addHost(f'h{j+1}', cpu=.5/n) for j in range(n)]
+
+        # Add links between switches
+        for i in range(n):
+            for j in range(i+1, n):
+                self.addLink(switches[i], switches[j], bw=10, delay='5ms', loss=10, max_queue_size=1000)
+
+        # Add links between hosts and switches
+        for i in range(n):
+            self.addLink(hosts[i], switches[i], bw=10, delay='5ms', loss=10, max_queue_size=1000)
+
+
+def perfTest():
+    if sys.argv[1] == "single":
+        topo = SingleSwitchTopo(int(sys.argv[2]))
+    elif sys.argv[1] =="linear":
+            topo = LinearTopo(int(sys.argv[2]))
+    elif sys.argv[1] =="tree":
+            topo = TreeTopo(int(sys.argv[2]))
+    elif sys.argv[1] =="mesh":
+            topo = MeshTopo(int(sys.argv[2]))
+    else:
+            print("unknown topology")
+
+    net = Mininet(topo, host = CPULimitedHost, link = TCLink)
+    net.start()
+    print("\n dumping host connections: \n")
+    dumpNodeConnections(net.hosts)
+    print("\n Testing network connectivity: \n")
+    net.pingAll();
+    print("\n Testing all pairwise bandwiths between hosts: \n")
+    for i in range(len(net.hosts)):
+            for j in range(i+1, len(net.hosts)):
+                net.iperf((net.get(f'h{i+1}'),net.get(f'h{j+1}')))
+    print("\n Stopping Network \n")
+    net.stop()
+
+if __name__=='__main__':
+    setLogLevel('info')
+    perfTest()
